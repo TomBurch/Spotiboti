7cd5dc6ab5c2545d62a20f6b7b0dd595cc12f2ea
BlartzelTheCat <thomas.burch1306@gmail.com>
2019-12-01 00:43:57 +0000

    Updated .play() to discord.py 1.3

--- music.py
+++ music.py
@@ -30,6 +30,48 @@ load_queues = {}
 full_queues = {}
 settings = {}
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume = 0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+        
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop = None, stream = False):
+        print(cls)
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download = not stream))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+        
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data = data)
+
+
 
 class Music(commands.Cog):
     def __init__(self, bot):
@@ -79,11 +121,13 @@ class Music(commands.Cog):
 
     @commands.command(pass_context = True)
     async def join(self, ctx):
-        channel = ctx.message.author.voice.channel
+        """Connects the bot to your voice channel"""
+
         server = ctx.message.author.guild
+        channel = ctx.message.author.voice.channel
         settings[server.id] = {'shuffle' : False}
-        await channel.connect();
-        #await client.join_voice_channel(channel)
+
+        await channel.connect()
         
     @commands.command(pass_context = True)
     async def leave(self, ctx):
@@ -91,14 +135,16 @@ class Music(commands.Cog):
 
         await ctx.voice_client.disconnect()
 
-    #@commands.command(pass_context = True)
-    #async def play(ctx, url):
-    #    server = ctx.message.server
-    #    voice_client = client.voice_client_in(server)
-    #    player = await voice_client.create_ytdl_player(url)
-    #    players[server.id] = player
+    @commands.command(pass_context = True)
+    async def play(self, ctx, *, url : str):
+        print(url)
+        server = ctx.message.guild
+        voice_channel = server.voice_client
 
-    #    player.start()
+        async with ctx.typing(): #Spotiboti is typing...
+            player = await YTDLSource.from_url(url, loop = self.bot.loop)
+            voice_channel.play(player, after = lambda e: print('Player error: %s' % e) if e else None)
+        await ctx.send('Now playing: {}'.format(player.title))
 
     #@commands.command(pass_context = True)
     #async def pause(ctx):
