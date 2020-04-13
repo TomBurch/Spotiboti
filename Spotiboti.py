626cfccb7e2ba83379b63be1d261cbf73a697738
TomBurch <thomas.burch1306@gmail.com>
2020-04-13 18:21:36 +0100

    Replaced run() with connect() and login()

--- Spotiboti.py
+++ Spotiboti.py
@@ -1,23 +1,55 @@
 import os
-
-from discord.ext import commands
+import asyncio
+from collections import namedtuple
 from dotenv import load_dotenv
+from discord.ext import commands
+
+
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix = '.')
+Entry = namedtuple('Entry', 'client event')
+bot = Entry(client = commands.Bot(command_prefix = '.'), 
+            event = asyncio.Event())
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 startup_extensions = ["admin", "music"]
 
-if __name__ == "__main__":
+def loadExtensions():
     for extension in startup_extensions:
         try:
-            bot.load_extension("cogs." + extension)
+            bot.client.load_extension("cogs." + extension)
             print("Loaded {} extension".format(extension))
         except Exception as e:
             print("Failed to load {} extension\n".format(extension))
             print(e) 
-            print("\n")
+            print("\n")  
 
-    bot.run(TOKEN)
+async def login():
+    await bot.client.login(TOKEN)
+
+async def connect():
+    try:
+        await bot.client.connect()
+    except Exception as e:
+        await bot.client.close()
+        print("Failed to connect")
+        print(e)
+        bot.event.set()
+
+async def checkClosed():
+    futures = [bot.event.wait()]
+    await asyncio.wait(futures)
+
+if __name__ == "__main__":
+    loadExtensions()
+    loop = asyncio.get_event_loop()
+
+    loop.run_until_complete(login())
+
+    loop.create_task(connect())
+
+    #Wait for client to close
+    loop.run_until_complete(checkClosed())
+
+    loop.close()
