3f73fa0417e8299b904ac5b70dda09a86d5aa039
TomBurch <thomas.burch1306@gmail.com>
2020-04-14 22:06:40 +0100

    Simplified connection, added logging

--- Spotiboti.py
+++ Spotiboti.py
@@ -3,12 +3,17 @@ import asyncio
 from collections import namedtuple
 from dotenv import load_dotenv
 from discord.ext import commands
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 load_dotenv()
 
-Entry = namedtuple('Entry', 'client event')
-bot = Entry(client = commands.Bot(command_prefix = '.'), 
-            event = asyncio.Event())
+bot = commands.Bot(command_prefix = '.')
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 startup_extensions = ["admin", "music"]
@@ -16,38 +21,18 @@ startup_extensions = ["admin", "music"]
 def loadExtensions():
     for extension in startup_extensions:
         try:
-            bot.client.load_extension("cogs." + extension)
+            bot.load_extension("cogs." + extension)
             print("Loaded {} extension".format(extension))
         except Exception as e:
             print("Failed to load {} extension\n".format(extension))
             print(e) 
             print("\n")  
 
-async def login():
-    await bot.client.login(TOKEN)
-
-async def connect():
-    try:
-        await bot.client.connect()
-    except Exception as e:
-        await bot.client.close()
-        print("Failed to connect")
-        print(e)
-        bot.event.set()
-
-async def checkClosed():
-    futures = [bot.event.wait()]
-    await asyncio.wait(futures)
-
 if __name__ == "__main__":
     loadExtensions()
-    loop = asyncio.get_event_loop()
 
-    loop.run_until_complete(login())
-
-    loop.create_task(connect())
-
-    #Wait for client to close
-    loop.run_until_complete(checkClosed())
-
-    loop.close()
+    while True:     
+        bot.loop.run_until_complete(bot.start(TOKEN))
+        
+        print("Reconnecting")
+        bot.client = commands.Bot(command_prefix = '.', loop = bot.loop)
