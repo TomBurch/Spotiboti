617eba10fcf60bef8f537123a1b9cc0c594c56d4
TomBurch <thomas.burch1306@gmail.com>
2020-04-15 18:44:47 +0100

    Get all tracks from playlist, not just 100

--- SpotiInteract/utility.py
+++ SpotiInteract/utility.py
@@ -5,22 +5,33 @@ import re
 def getPlaylistFromId(id, access_token):
     URL = "https://api.spotify.com/v1/playlists/" + id + "/tracks"
     headers = {"Authorization": "Bearer " + access_token}
-    r = requests.get(URL, headers = headers, verify = True)
+    params = {"offset": 0}
     playlist = []
     
-    if r.status_code != 200:
-        print("Spotify GET error:")
-        print(r.reason)
-        print(r.text)
-        print(r)
-        return False
+    while True:
+        r = requests.get(URL, headers = headers, verify = True, params = params)
 
-    tracks = r.json()['items']
-    for track in tracks:
-        track = track['track']
-        song = '{} - {}'.format(track['artists'][0]['name'], track['name'])
-        song = re.sub('[/]', ' ', song) # '/' in string causes http errors
-        playlist.append(song)
+        if r.status_code != 200:
+            print(r.status_code)
+            print("Spotify GET error:")
+            print(r.reason)
+            print(r.text)
+            print(r)
+            return 
+
+        songs = []
+        tracks = r.json()['items']
+        for track in tracks:
+            track = track['track']
+            song = '{} - {}'.format(track['artists'][0]['name'], track['name'])
+            song = re.sub('[/]', ' ', song) # '/' in string causes http errors
+            songs.append(song)
+        
+        if (songs == []):
+            break
+
+        playlist += songs
+        params["offset"] = params["offset"] + 100
 
     return playlist
 
