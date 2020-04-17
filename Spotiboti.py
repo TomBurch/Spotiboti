3588a3e77b238a28b5f964ccd99289a09c0b2633
TomBurch <thomas.burch1306@gmail.com>
2020-04-17 19:10:47 +0100

    Removed imports

--- Spotiboti.py
+++ Spotiboti.py
@@ -1,9 +1,8 @@
 import os
-import asyncio
-from collections import namedtuple
 from dotenv import load_dotenv
-from discord.ext import commands
 import logging
+
+from discord.ext import commands
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
